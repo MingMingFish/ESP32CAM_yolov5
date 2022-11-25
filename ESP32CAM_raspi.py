c1c7553296136bdcfc5a12dcd1a1ee8f5f73573d
MingMingFish <88556981+MingMingFish@users.noreply.github.com>
2022-11-25 17:21:23 +0800

    update raspi version
    to avoid the warning spamming, use the .sh for first run

--- ESP32CAM_raspi.py
+++ ESP32CAM_raspi.py
@@ -1,5 +1,7 @@
+import warnings
 import os
 import time
+import numpy as np
 from urllib.request import urlopen
 import torch
 import cv2
@@ -9,7 +11,7 @@ import pygame
 pygame.init()
 
 #ESP32-CAM
-url = 'http://192.168.1.101'
+url = 'http://192.168.131.134'
 stream_url= f"{url}:81/stream"
 CAMERA_BUFFER_SIZE=4096
 
@@ -21,64 +23,95 @@ XGA  = 10 # 1024 *  768
 SVGA = 9  #  800 *  600
 VGA  = 8  #  640 *  480
 
-#模型設定
-model = torch.hub.load(repo_or_dir='yolov5',model='yolov5n',source='local') # s/m/l/x
+#set/load yolov5 model
+model = torch.hub.load(repo_or_dir='yolov5',model='yolov5n',source='local') # n/s/m/l/x
 
 def read_stream():
     global bts
+    for _ in iter(int, 1):
+        bts += stream.read(CAMERA_BUFFER_SIZE)
+        jpghead = bts.find(b'\xff\xd8')
+        jpgend = bts.find(b'\xff\xd9')
 
-    for _ in iter(int, 1): # infinite loop:
-        bts+=stream.read(CAMERA_BUFFER_SIZE)
-        jpghead=bts.find(b'\xff\xd8')
-        jpgend=bts.find(b'\xff\xd9')
-        #print(f"jpghead: {jpghead}, jpgend: {jpgend}")
+        img = None
+        height, width = 0, 0
+        
+        if jpghead >-1 and jpgend >- 1:
+            jpg = bts[jpghead:jpgend+2]
+            bts = bts[jpgend+2:]
 
-        if jpghead>-1 and jpgend>-1:
-            jpg=bts[jpghead:jpgend+2]
-            bts=bts[jpgend+2:]
+            try:
+                img=cv2.imdecode(np.frombuffer(jpg,dtype=np.uint8),cv2.IMREAD_UNCHANGED)
+                height, width = img.shape[:2]
+            except:
+                continue
             break
-    return jpg
+    return img, (width, height)
 
-bts=b''
+timers = {}
+bts = b''
+
 if __name__ == "__main__":
+    warnings.simplefilter("ignore")
     try:
         for _ in iter(int, 1): # infinite loop:
             try:
-                print('Connecting ESP32-CAM from ',stream_url)
-                urlopen(f'{url}/control?var=framesize&val={HD}') # set graph quality
+                print('Connecting ESP32-CAM from',stream_url)
+                urlopen(f'{url}/control?var=framesize&val={VGA}') # set graph quality
                 stream=urlopen(stream_url)
                 break
             except:
                 print("Connect Failed.")
-                tryagain=input('Try again?(Y/[N]): ').capitalize()
-                if tryagain =='Y':
+                try_again=input('Try again?(Y/[N]):').capitalize()
+                if try_again =='Y':
                     continue
                 else:
                     os._exit(0)
-        print('Connected ESP32 from ',stream_url)
+        print('Connected ESP32 from',stream_url)
 
-        timers =    {'person': 0, 'bicycle': 0, 'car': 0, 'motorcycle': 0, 'airplane': 0, 'bus': 0, 'train': 0, 'truck': 0, 'boat': 0, 'traffic light': 0, 'fire hydrant': 0, 'stop sign': 0, 
-                    'parking meter': 0,'bench': 0, 'bird': 0, 'cat': 0, 'dog': 0, 'horse': 0, 'sheep': 0, 'cow': 0, 'elephant': 0, 'bear': 0,'zebra': 0, 'giraffe': 0, 'backpack': 0, 
-                    'umbrella': 0, 'handbag': 0, 'tie': 0, 'suitcase': 0, 'frisbee': 0, 'skis': 0, 'snowboard': 0, 'sports ball': 0, 'kite': 0, 'baseball bat': 0, 'baseball glove': 0, 
-                    'skateboard': 0, 'surfboard': 0, 'tennis racket': 0,'bottle': 0, 'wine glass': 0, 'cup': 0, 'fork': 0, 'knife': 0, 'spoon': 0, 'bowl': 0, 'banana': 0, 'apple': 0,
-                    'sandwich': 0, 'orange': 0, 'broccoli': 0, 'carrot': 0,'hot dog': 0, 'pizza': 0, 'donut': 0, 'cake': 0, 'chair': 0, 'couch': 0, 'potted plant': 0, 'bed': 0, 'dining table': 0,
-                    'toilet': 0, 'tv': 0, 'laptop': 0, 'mouse': 0, 'remote': 0, 'keyboard': 0, 'cell phone': 0, 'microwave': 0, 'oven': 0, 'toaster': 0, 'sink': 0, 'refrigerator': 0, 
-                    'book': 0, 'clock': 0, 'vase': 0, 'scissors': 0, 'teddy bear': 0, 'hair drier': 0, 'toothbrush': 0}
         for _ in iter(int, 1): # infinite loop:
-            frame = read_stream()
-            results = model(frame)
-            for r in results:
-                if time.time - timers[r] >= 5:
-                        timers[r] = time.time()
-                        sound = pygame.mixer.Sound(os.path.join('audio', r))
-                        sound.play()
+            frame, (width, height) = read_stream()
+            alert_vol = (width*height)//4
 
-            if frame is not None:
-                cv2.imshow("ESP32-CAM", frame)
+            results = model(frame)
+            #get result items
+            items = eval(results.pandas().xyxy[0].to_json(orient="records"))
+            #Count items
+            item_count = len(items)
+
+            items_name =[]
+            for item in items:
+                # item['xmin', 'name', 'ymin', 'xmax', 'ymax', 'confidence', 'class', 'name']
+                volume = int(item['xmax'] - item['xmin']) * int(item['ymax'] - item['ymin'])    # 計算體積
+                if volume >= alert_vol and item['confidence'] >= 0.5:
+                    items_name.append(item['name'])
+                    if time.time() - timers.setdefault(item['name'], time.time()) >= 3:
+                        timers[item['name']] = time.time()
+                        try:
+                            sound = pygame.mixer.Sound(os.path.join('audio', item['name'] + '.mp3'))
+                        except FileNotFoundError:
+                            print('not found in dictionary')
+                            sound = pygame.mixer.Sound(os.path.join('audio', 'others.mp3')) # not added yet
+                        finally:
+                            sound.play()
+
+            # Show detections (only big enough ones)
+            if items_name:
+                print(items_name)
+
+            # Render the new frame of detections
+            new_frame = np.squeeze(results.render())
+            
+            # Show frame
+            if new_frame is not None:
+                cv2.imshow("ESP32-CAM", new_frame)
                 k=cv2.waitKey(1)
+            
+            # Break when press 'q'
             if k & 0xFF==ord('q'):
                 cv2.destroyAllWindows()
                 break
+    # when pressed Ctrl+C
     except KeyboardInterrupt:
         print("Application broke down by user")
         os.system("pause")
---
+++ run_on_raspi.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/pi/ESP32CAM_yolov5
+export OMP_NUM_THREADS=1
+python ESP32CAM_raspi.py
